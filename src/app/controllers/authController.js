4a4795474193563359dc80a510d1ae9449285e64
Marcella <marcellatsangos@gmail.com>
2020-05-25 09:57:50 -0300

    Organizing folders

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -2,7 +2,7 @@ const express = require('express') // Sempre que for mexer com rotas.
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-const authConfig = require('./../config/auth')
+const authConfig = require('./../../config/auth')
 
 const User = require('../models/User') // Necessário p/ ações de login e cadastro.
 
